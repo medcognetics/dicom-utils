b682c1852154bef267cea9bfa9c7b87ba106882b
Tim <timothycogan@gmail.com>
2021-06-12 21:47:24 -0500

    Add folder support to dicom2img (display all folder images in collage)

--- dicom_utils/cli/dicom2img.py
+++ dicom_utils/cli/dicom2img.py
@@ -3,19 +3,21 @@
 import argparse
 from argparse import ArgumentParser
 from pathlib import Path
-from typing import Optional
+from typing import List, Optional
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pydicom
+from numpy import ndarray
 from PIL import Image
 
-from ..dicom import read_dicom_image
+from ..dicom import has_dicm_prefix, read_dicom_image
+from ..logging import logger
 from ..types import Dicom
 
 
 def get_parser(parser: ArgumentParser = ArgumentParser()) -> ArgumentParser:
-    parser.add_argument("file", help="DICOM file to convert")
+    parser.add_argument("path", help="DICOM file or folder with files to convert")
     parser.add_argument("-o", "--output", help="output filepath. if directory, use filename from `file`")
     parser.add_argument(
         "-s", "--split", default=False, action="store_true", help="split multi-frame inputs into separate files"
@@ -26,10 +28,50 @@ def get_parser(parser: ArgumentParser = ArgumentParser()) -> ArgumentParser:
     return parser
 
 
+def to_collage(images: List[ndarray]) -> ndarray:
+    num_images = len(images)
+
+    assert num_images != 0, "There must be at least one image."
+    assert all(len(i.shape) == 3 for i in images), "The images must have 3 channels."
+
+    image_chns, max_image_rows, max_image_cols = np.array([i.shape for i in images]).max(axis=0)
+
+    collage_rows = 1 if num_images < 3 else 2
+    collage_cols = int(num_images / collage_rows + 0.5)
+
+    collage = np.zeros((image_chns, collage_rows * max_image_rows, collage_cols * max_image_cols))
+
+    for i, image in enumerate(images):
+        row = int(i >= collage_cols)
+        col = i % collage_cols
+        start_row = row * max_image_rows
+        start_col = col * max_image_cols
+        image_chns, image_rows, image_cols = image.shape
+        collage[:image_chns, start_row : start_row + image_rows, start_col : start_col + image_cols] = image
+
+    return collage
+
+
+def dcms_to_image(dcms: List[Dicom]) -> ndarray:
+    valid_image_dcms = []
+    for dcm in dcms:
+        try:
+            valid_image_dcms.append(read_dicom_image(dcm))
+        except Exception as e:
+            logger.info(e)
+
+    return to_collage(valid_image_dcms)
+
+
 def dicom_to_image(
-    dcm: Dicom, dest: Optional[Path] = None, split: bool = False, fps: int = 5, quality: int = 95, block: bool = True
+    dcms: List[Dicom],
+    dest: Optional[Path] = None,
+    split: bool = False,
+    fps: int = 5,
+    quality: int = 95,
+    block: bool = True,
 ) -> None:
-    data = read_dicom_image(dcm)
+    data = dcms_to_image(dcms)
 
     # min max norm
     min, max = data.min(), data.max()
@@ -38,7 +80,7 @@ def dicom_to_image(
     H, W = data.shape[-2:]
 
     # single image
-    if dcm.pixel_array.ndim == 2:
+    if dcms[0].pixel_array.ndim == 2:
         data = data[0]  # drop channel
         if dest is None:
             plt.imshow(data.reshape(H, W), cmap="gray")
@@ -67,11 +109,7 @@ def dicom_to_image(
                 break
 
     else:
-        frames = []
-        for frame in data:
-            img = Image.fromarray(frame)
-            frames.append(img)
-
+        frames = [Image.fromarray(frame) for frame in data]
         duration_ms = len(frames) / (fps * 1000)
         if dest is None:
             raise NotImplementedError("3D inputs with no `dest` is not yet supported")
@@ -80,19 +118,25 @@ def dicom_to_image(
             frames[0].save(path, save_all=True, append_images=frames[1:], duration=duration_ms, quality=quality)
 
 
-def main(args: argparse.Namespace) -> None:
-    source = Path(args.file)
-    dest = Path(args.output) if args.output is not None else None
+def path_to_sources(path: Path) -> List[Path]:
+    if path.is_dir():
+        return [f for f in path.iterdir() if has_dicm_prefix(f)]
+    if path.is_file():
+        return [path]
+    else:
+        raise FileNotFoundError(path)
 
-    if not source.is_file():
-        raise FileNotFoundError(source)
+
+def main(args: argparse.Namespace) -> None:
+    path = Path(args.path)
+    dest = Path(args.output) if args.output is not None else None
 
     # handle case where output path is a dir
     if dest is not None and dest.is_dir():
-        dest = Path(dest, source.stem).with_suffix(".png")
+        dest = Path(dest, path.stem).with_suffix(".png")
 
-    with pydicom.dcmread(source) as dcm:
-        dicom_to_image(dcm, dest, args.split, args.fps, args.quality, not args.noblock)
+    dcms = [pydicom.dcmread(source) for source in path_to_sources(path)]
+    dicom_to_image(dcms, dest, args.split, args.fps, args.quality, not args.noblock)
 
 
 def entrypoint():
