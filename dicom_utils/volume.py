8ded86c3da5e6191b45e983ab89eb3ad1732b299
Scott Chase Waggener <tidalpaladin@protonmail.com>
2021-10-08 12:44:22 -0500

    Allow slicing 3D data without decompressing

--- dicom_utils/volume.py
+++ dicom_utils/volume.py
@@ -2,18 +2,77 @@
 # -*- coding: utf-8 -*-
 
 from abc import ABC, abstractmethod
-from typing import Sized, SupportsIndex, TypeVar
+from copy import deepcopy
+from itertools import islice
+from typing import Iterator, Optional, Sized, SupportsIndex, SupportsInt, Tuple, TypeVar, Union, overload
+
+import numpy as np
+from pydicom import Dataset
+from pydicom.encaps import encapsulate, generate_pixel_data_frame
 
 
 T = TypeVar("T", bound=SupportsIndex)
+U = TypeVar("U", bound=Dataset)
 
 
 class VolumeHandler(ABC):
     r"""Base class for classes that manipulate 3D Volumes"""
 
     @abstractmethod
+    def get_indices(self, total_frames: Optional[int]) -> Tuple[int, int, int]:
+        ...
+
+    @overload
     def __call__(self, x: T) -> T:
         ...
+
+    @overload
+    def __call__(self, x: U) -> U:
+        ...
+
+    def __call__(self, x: Union[T, U]) -> Union[T, U]:
+        if isinstance(x, Dataset):
+            return self.slice_dicom(x)
+        else:
+            return self.slice_array(x)
+
+    def slice_array(self, x: T) -> T:
+        r"""Slices an array input according to :func:`get_indices`"""
+        num_frames = len(x) if isinstance(x, Sized) else None
+        start, stop, stride = self.get_indices(num_frames)
+        result = x[start:stop:stride]  # type: ignore
+        return result
+
+    def slice_dicom(self, dcm: U) -> U:
+        r"""Slices a DICOM object input according to :func:`get_indices`.
+
+        .. note:
+            Unlike :func:`slice_array`, this function can perform slicing on compressed DICOMs
+            with out needing to decompress all frames. This can provide a substantial performance gain.
+
+        """
+        # copy dicom and read key tags
+        dcm = deepcopy(dcm)
+        num_frames: Optional[SupportsInt] = dcm.get("NumberOfFrames", None)  # type: ignore
+        num_frames = int(num_frames) if num_frames is not None else None
+        is_compressed: bool = dcm.file_meta.TransferSyntaxUID.is_compressed  # type: ignore
+
+        start, stop, stride = self.get_indices(num_frames)
+
+        # read data
+        if is_compressed:
+            all_frames: Iterator = generate_pixel_data_frame(dcm.PixelData, num_frames)  # type: ignore
+            frames = list(islice(all_frames, start, stop, stride))
+            new_pixel_data = encapsulate(frames)
+        else:
+            all_frames: np.ndarray = dcm.pixel_array
+            frames = all_frames[start:stop:stride]
+            new_pixel_data = frames.tobytes()
+
+        out_frames = len(frames)
+        dcm.NumberOfFrames = out_frames
+        dcm.PixelData = new_pixel_data
+        return dcm
 
 
 class KeepVolume(VolumeHandler):
@@ -22,8 +81,8 @@ class KeepVolume(VolumeHandler):
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}()"
 
-    def __call__(self, x: T) -> T:
-        return x
+    def get_indices(self, total_frames: Optional[int]) -> Tuple[int, Optional[int], int]:
+        return 0, None, 1
 
 
 class SliceAtLocation(VolumeHandler):
@@ -63,11 +122,10 @@ class SliceAtLocation(VolumeHandler):
         s += ")"
         return s
 
-    def __call__(self, x: T) -> T:
+    def get_indices(self, total_frames: Optional[int]) -> Tuple[int, Optional[int], int]:
         start = self.center - self.before
         end = self.center + self.after + 1
-        result = x[start : end : self.stride]  # type: ignore
-        return result
+        return start, end, self.stride
 
 
 class UniformSample(VolumeHandler):
@@ -100,14 +158,12 @@ class UniformSample(VolumeHandler):
         s += ")"
         return s
 
-    def __call__(self, x: T) -> T:
+    def get_indices(self, total_frames: Optional[int]) -> Tuple[int, Optional[int], int]:
         if self.method == "stride":
-            result = x[:: self.amount]  # type: ignore
+            return 0, None, self.amount
         elif self.method == "count":
-            assert isinstance(x, Sized)
-            total_slices = len(x)
-            stride = max(total_slices // self.amount, 1)
-            result = x[::stride]  # type: ignore
+            assert total_frames is not None
+            stride = max(total_frames // self.amount, 1)
+            return 0, None, stride
         else:
             raise ValueError(f"Invalid method {self.method}")
-        return result
--- tests/test_volume.py
+++ tests/test_volume.py
@@ -7,49 +7,102 @@ import pytest
 from dicom_utils import KeepVolume, SliceAtLocation, UniformSample
 
 
-def test_keep_volume():
-    x = np.random.rand(10, 10)
-    sampler = KeepVolume()
-    result = sampler(x)
-    assert (x == result).all()
+class TestKeepVolume:
+    def test_array(self):
+        x = np.random.rand(10, 10)
+        sampler = KeepVolume()
+        result = sampler(x)
+        assert (x == result).all()
+
+    def test_dicom(self, dicom_object_3d, transfer_syntax):
+        N = 8
+        dcm = dicom_object_3d(N, syntax=transfer_syntax)
+        sampler = KeepVolume()
+        result = sampler(dcm)
+        assert type(result) == type(dcm)
+        assert result.NumberOfFrames == dcm.NumberOfFrames
+        assert (result.pixel_array == dcm.pixel_array).all()
 
 
-@pytest.mark.parametrize(
-    "center,before,after,stride,index",
-    [
-        pytest.param(5, 0, 0, 1, lambda a: a[5]),
-        pytest.param(5, 1, 0, 1, lambda a: a[4:6]),
-        pytest.param(5, 1, 1, 1, lambda a: a[4:7]),
-        pytest.param(5, 0, 0, 2, lambda a: a[5]),
-        pytest.param(5, 2, 2, 2, lambda a: a[3:8:2]),
-    ],
-)
-def test_slice_at_location(center, before, after, stride, index):
-    x = np.random.rand(10, 10)
-    sampler = SliceAtLocation(center, before, after, stride)
-    result = sampler(x)
-    assert type(x) == type(result)
-    assert (index(x) == result).all()
+class TestSliceAtLocation:
+    @pytest.mark.parametrize(
+        "center,before,after,stride,index",
+        [
+            pytest.param(5, 0, 0, 1, lambda a: a[5]),
+            pytest.param(5, 1, 0, 1, lambda a: a[4:6]),
+            pytest.param(5, 1, 1, 1, lambda a: a[4:7]),
+            pytest.param(5, 0, 0, 2, lambda a: a[5]),
+            pytest.param(5, 2, 2, 2, lambda a: a[3:8:2]),
+        ],
+    )
+    def test_array(self, center, before, after, stride, index):
+        x = np.random.rand(10, 10)
+        sampler = SliceAtLocation(center, before, after, stride)
+        result = sampler(x)
+        assert type(x) == type(result)
+        assert (index(x) == result).all()
+
+    @pytest.mark.parametrize(
+        "center,before,after,stride",
+        [
+            pytest.param(5, 0, 0, 1),
+            pytest.param(5, 1, 0, 1),
+            pytest.param(5, 1, 1, 1),
+            pytest.param(5, 0, 0, 2),
+            pytest.param(5, 2, 2, 2),
+        ],
+    )
+    def test_dicom(self, dicom_object_3d, center, before, after, stride, transfer_syntax):
+        N = 8
+        dcm = dicom_object_3d(N, syntax=transfer_syntax)
+
+        sampler = SliceAtLocation(center, before, after, stride)
+        result = sampler(dcm)
+        assert dcm.NumberOfFrames == N, "the input dicom object was modified"
+        assert type(result) == type(dcm)
+        assert (sampler(dcm.pixel_array) == result.pixel_array).all()
 
 
-@pytest.mark.parametrize(
-    "size,amount,method,index",
-    [
-        pytest.param(10, 2, "stride", lambda a: a[::2]),
-        pytest.param(12, 2, "stride", lambda a: a[::2]),
-        pytest.param(10, 4, "count", lambda a: a[:: 10 // 4]),
-        pytest.param(12, 4, "count", lambda a: a[:: 12 // 4]),
-        pytest.param(2, 4, "count", lambda a: a),
-        pytest.param(2, 1, "stride", lambda a: a),
-    ],
-)
-def test_uniform_sample(size, amount, method, index):
-    x = np.random.rand(size, 10)
-    sampler = UniformSample(amount, method)
-    result = sampler(x)
-    assert type(x) == type(result)
-    expected = index(x)
-    try:
-        assert (expected == result).all()
-    except Exception:
-        assert expected == result
+class TestUniformSample:
+    @pytest.mark.parametrize(
+        "size,amount,method,index",
+        [
+            pytest.param(10, 2, "stride", lambda a: a[::2]),
+            pytest.param(12, 2, "stride", lambda a: a[::2]),
+            pytest.param(10, 4, "count", lambda a: a[:: 10 // 4]),
+            pytest.param(12, 4, "count", lambda a: a[:: 12 // 4]),
+            pytest.param(2, 4, "count", lambda a: a),
+            pytest.param(2, 1, "stride", lambda a: a),
+        ],
+    )
+    def test_array(self, size, amount, method, index):
+        x = np.random.rand(size, 10)
+        sampler = UniformSample(amount, method)
+        result = sampler(x)
+        assert type(x) == type(result)
+        expected = index(x)
+        try:
+            assert (expected == result).all()
+        except Exception:
+            assert expected == result
+
+    @pytest.mark.parametrize(
+        "amount,method",
+        [
+            pytest.param(2, "stride"),
+            pytest.param(2, "stride"),
+            pytest.param(4, "count"),
+            pytest.param(4, "count"),
+            pytest.param(4, "count"),
+            pytest.param(1, "stride"),
+        ],
+    )
+    def test_dicom(self, dicom_object_3d, amount, method, transfer_syntax):
+        N = 8
+        dcm = dicom_object_3d(N, syntax=transfer_syntax)
+
+        sampler = UniformSample(amount, method)
+        result = sampler(dcm)
+        assert dcm.NumberOfFrames == N, "the input dicom object was modified"
+        assert type(result) == type(dcm)
+        assert (sampler(dcm.pixel_array) == result.pixel_array).all()
