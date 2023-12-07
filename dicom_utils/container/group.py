a9ee46c2c020ce344367003f353c12183967d34a
Scott Chase Waggener <tidalpaladin@protonmail.com>
2023-12-07 16:38:36 -0600

    Apply collection helpers at top group level (#108)
    * Apply collection helpers at top group level
    
    * Add constants

--- dicom_utils/container/group.py
+++ dicom_utils/container/group.py
@@ -7,7 +7,7 @@ import warnings
 from dataclasses import dataclass, field
 from functools import partial
 from pathlib import Path
-from typing import Callable, Dict, Hashable, Iterator, Optional, Sequence, Tuple, TypeVar, cast
+from typing import Callable, Dict, Final, Hashable, Iterator, Optional, Sequence, Tuple, TypeVar, cast
 
 from registry import Registry
 from tqdm_multiprocessing import ConcurrentMapper
@@ -27,6 +27,8 @@ GroupFunction = Callable[[FileRecord], Hashable]
 GROUP_REGISTRY = Registry("group", bound=Callable[..., Hashable])
 Key = Tuple[Hashable, ...]
 GroupDict = Dict[Key, RecordCollection]
+COMPLETE_COLLECTION_INDEX: Final[int] = 0
+FIRST_GROUP_INDEX: Final[int] = 1
 
 logger = logging.getLogger(__name__)
 
@@ -94,6 +96,9 @@ class Grouper:
         result: Dict[Key, RecordCollection] = {tuple(): collection}
 
         with ConcurrentMapper(self.threads, self.jobs, chunksize=self.chunksize, timeout=self.timeout) as mapper:
+            # Apply helpers at the entire collection level
+            collection = apply_helpers(collection, self._helper_fns, index=COMPLETE_COLLECTION_INDEX)
+
             for i, group_fn in list(enumerate(self._group_fns)):
                 # run the group function
                 total = sum(len(v) for v in result.values())
@@ -120,7 +125,7 @@ class Grouper:
                     leave=True,
                     total=len(result),
                 )
-                mapped = mapper(self._apply_helpers, list(result.items()), index=i)
+                mapped = mapper(self._apply_helpers, list(result.items()), index=i + 1)
                 result = {k: v for k, v in mapped}
                 mapper.close_bar()
 
